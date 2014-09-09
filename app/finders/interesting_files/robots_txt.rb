694649dab84bfa2fe6ff5faa36324a37e2258ef1
erwanlr <erwan.lr@gmail.com>
2014-09-09 17:02:43 +0200

    Fixes a regex in Finders::RobotsTxt

--- app/finders/interesting_files/robots_txt.rb
+++ app/finders/interesting_files/robots_txt.rb
@@ -13,7 +13,7 @@ module CMSScanner
           res = Typhoeus.get(url)
 
           return unless res && res.code == 200
-          return unless res.body =~ /(?:user-agent|disallow):/i
+          return unless res.body =~ /(?:user-agent|(?:dis)?allow):/i
 
           CMSScanner::InterestingFile.new(url, confidence: 100, found_by: found_by)
         end
