9d4fa7f3b75ddef7f9af96607e367376656177a7
erwanlr <erwan.lr@gmail.com>
2014-09-29 18:31:39 +0200

    Adds XML RPC call to Typhoeus

--- lib/cms_scanner/browser/actions.rb
+++ lib/cms_scanner/browser/actions.rb
@@ -34,6 +34,33 @@ module CMSScanner
         get(url, params.merge(followlocation: true))
       end
 
+      # @parm [ String ] url
+      # @param [ String ] rpc_method
+      # @param [ Array ] rpc_params
+      # @param [ Hash ] request_params
+      #
+      # @return [ Typhoeus::Response ]
+      def xml_rpc_call(url, rpc_method, rpc_params = [], request_params = {})
+        post(url, request_params.merge(body: xml_rpc_body(rpc_method, rpc_params)))
+      end
+
+      # Might be better to use Nokogiri to create the XML body ?
+      #
+      # @param [ String ] method
+      # @param [ Array ] params
+      #
+      # @return [ String ] The body of the XML RPC request
+      def xml_rpc_body(method, params = [])
+        p_body = ''
+
+        params.each { |p| p_body << "<param><value><string>#{p}</string></value></param>" }
+
+        body = '<?xml version="1.0"?><methodCall>'
+        body << "<methodName>#{method}</methodName>"
+        body << "<params>#{p_body}</params>" unless p_body.length == 0
+        body << '</methodCall>'
+      end
+
       protected
 
       # @param [ String ] url
--- spec/shared_examples/browser_actions.rb
+++ spec/shared_examples/browser_actions.rb
@@ -1,14 +1,15 @@
 
 shared_examples CMSScanner::Browser::Actions do
 
-  let(:url) { 'http://example.com/file.txt' }
+  let(:url)     { 'http://example.com/file.txt' }
+  let(:browser) { CMSScanner::Browser }
 
   describe '#get, #post, #head' do
     [:get, :post, :head].each do |method|
       it 'calls the method and returns a Typhoeus::Response' do
         stub_request(method, url)
 
-        expect(CMSScanner::Browser.send(method, url)).to be_a Typhoeus::Response
+        expect(browser.send(method, url)).to be_a Typhoeus::Response
       end
     end
   end
@@ -20,11 +21,51 @@ shared_examples CMSScanner::Browser::Actions do
       stub_request(:get, url).to_return(status: 301, headers: { location: redirection })
       stub_request(:get, redirection).to_return(status: 200, body: 'Got me')
 
-      response = CMSScanner::Browser.get_and_follow_location(url)
+      response = browser.get_and_follow_location(url)
       expect(response).to be_a Typhoeus::Response
       # Line below is not working due to an issue in Typhoeus/Webmock
       # See https://github.com/typhoeus/typhoeus/issues/279
       # expect(response.body).to eq 'Got me'
+    end
+  end
+
+  describe '#xml_rpc_body' do
+    after { expect(browser.xml_rpc_body(method, params)).to eq @expected }
+
+    let(:method) { 'rpc-test' }
+    let(:params) { [] }
+
+    context 'when no params' do
+      it 'returns the correct body' do
+        @expected = '<?xml version="1.0"?><methodCall>'
+        @expected << "<methodName>#{method}</methodName>"
+        @expected << '</methodCall>'
+      end
+    end
+
+    context 'when params' do
+      let(:params) { %w(p1 p2) }
+
+      it 'returns the right body' do
+        @expected = '<?xml version="1.0"?><methodCall>'
+        @expected << "<methodName>#{method}</methodName><params>"
+        @expected << '<param><value><string>p1</string></value></param>'
+        @expected << '<param><value><string>p2</string></value></param>'
+        @expected << '</params></methodCall>'
+      end
+    end
+  end
+
+  describe '#xml_rpc_call' do
+    let(:rpc_method) { 'rpc-test' }
+
+    it 'returns a Typhoeus::Response' do
+      stub_request(:post, url).with(body: browser.xml_rpc_body(rpc_method)).to_return(body: 'OK')
+
+      response = browser.xml_rpc_call(url, rpc_method)
+
+      expect(response).to be_a Typhoeus::Response
+      expect(response.body).to eq 'OK'
     end
   end
 
