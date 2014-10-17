c81f70d426c2c6985a8fae51501795219b775be3
erwanlr <erwan.lr@gmail.com>
2014-10-17 17:58:17 +0200

    Adds wordpress? & wordpress_hosted? for the WordPress platform

--- lib/cms_scanner/target/platform/wordpress.rb
+++ lib/cms_scanner/target/platform/wordpress.rb
@@ -9,10 +9,25 @@ module CMSScanner
       module WordPress
         include PHP
 
+        WORDPRESS_PATTERN = %r{/(?:(?:wp-content/(?:themes|plugins|plugins))|wp-includes)/}i
+
         def wordpress?
+          page = Nokogiri::HTML(Browser.get(url).body)
+
+          page.css('script, link').each do |tag|
+            tag_url = tag.attribute('href').to_s
+
+            next unless in_scope?(tag_url)
+
+            tag_uri = Addressable::URI.parse(tag_url)
+
+            return true if tag_uri.path =~ WORDPRESS_PATTERN
+          end
+          false
         end
 
         def wordpress_hosted?
+          uri.host =~ /wordpress.com$/i ? true : false
         end
       end
     end
