a230f13aad3808bd6e0c24c56b0c1a28a0ea7acf
erwanlr <erwan.lr@gmail.com>
2014-10-31 17:36:57 +0100

    .

--- spec/shared_examples/views/core.rb
+++ spec/shared_examples/views/core.rb
@@ -2,7 +2,7 @@
 shared_examples 'App::Views::Core' do
 
   let(:controller) { CMSScanner::Controller::Core.new }
-  let(:tpl_vars)   { { url: target_url, start_time: Time.parse('2014-10-30 13:02:01 DST') } }
+  let(:tpl_vars)   { { url: target_url, start_time: Time.parse('2014-10-30 13:02:01 +0100 DST') } }
 
   describe 'started' do
     let(:view) { 'started' }
@@ -17,7 +17,7 @@ shared_examples 'App::Views::Core' do
 
     it 'outputs the expected string' do
       @tpl_vars = tpl_vars.merge(
-        stop_time: Time.parse('2014-10-30 13:02:03 DST'),
+        stop_time: Time.parse('2014-10-30 13:02:03 +0100 DST'),
         used_memory: 100,
         elapsed: 3
       )
