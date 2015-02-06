8d96f50d1eab29a3ed1d2cce547531189452a0bf
erwanlr <erwan.lr@gmail.com>
2015-02-06 21:25:10 +0100

    Adds Target#*_hash

--- lib/cms_scanner/target.rb
+++ lib/cms_scanner/target.rb
@@ -2,6 +2,7 @@ require 'cms_scanner/web_site'
 require 'cms_scanner/target/platform'
 require 'cms_scanner/target/server'
 require 'cms_scanner/target/scope'
+require 'cms_scanner/target/hashes'
 
 module CMSScanner
   # Target to Scan
---
+++ lib/cms_scanner/target/hashes.rb
@@ -0,0 +1,39 @@
+module CMSScanner
+  # Scope system logic
+  class Target < WebSite
+    # @note Comments are deleted to avoid cache generation details
+    #
+    # @param [ Typhoeus::Response, String ] page
+    #
+    # @return [ String ] The md5sum of the page
+    def self.page_hash(page)
+      page = NS::Browser.get(page, followlocation: true) unless page.is_a?(Typhoeus::Response)
+
+      Digest::MD5.hexdigest(page.body.gsub(/<!--.*?-->/m, ''))
+    end
+
+    # @return [ String ] The hash of the homepage
+    def homepage_hash
+      @homepage_hash ||= Target.page_hash(url)
+    end
+
+    # @note This is used to detect potential custom 404 responding with a 200
+    # @return [ String ] The hash of a 404
+    def error_404_hash
+      @error_404_hash ||= Target.page_hash(non_existant_page_url)
+    end
+
+    # @return [ String ] The URL of an unlikely existant page
+    def non_existant_page_url
+      uri.join(Digest::MD5.hexdigest(rand(999_999_999).to_s) + '.html').to_s
+    end
+
+    # @param [ Typhoeus::Response, String ] page
+    # @return [ Boolean ] Wether or not the page is a the homepage or a 404 based on its md5sum
+    def homepage_or_404?(page)
+      md5sum = Target.page_hash(page)
+
+      md5sum == homepage_hash || md5sum == error_404_hash
+    end
+  end
+end
