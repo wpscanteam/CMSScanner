66f577789661297e695351334dcd008ca78b12f5
erwanlr <erwan.lr@gmail.com>
2015-02-13 15:52:07 +0100

    Adds more colour methods

--- app/formatters/cli.rb
+++ app/formatters/cli.rb
@@ -2,12 +2,24 @@ module CMSScanner
   module Formatter
     # CLI Formatter
     class Cli < Base
+      def bold(text)
+        colorize(text, 1)
+      end
+
       def red(text)
         colorize(text, 31)
       end
 
       def green(text)
         colorize(text, 32)
+      end
+
+      def amber(text)
+        colorize(text, 33)
+      end
+
+      def blue(text)
+        colorize(text, 34)
       end
 
       def colorize(text, color_code)
--- spec/app/formatters/cli_spec.rb
+++ spec/app/formatters/cli_spec.rb
@@ -7,13 +7,25 @@ describe CMSScanner::Formatter::Cli do
     its(:format) { should eq 'cli' }
   end
 
-  describe '#green, #red, #colorize' do
+  describe '#bold, #red, #green, #amber, #blue, #colorize' do
+    it 'returns the correct bold string' do
+      expect(formatter.bold('Text')).to eq "\e[1mText\e[0m"
+    end
+
     it 'returns the correct red string' do
       expect(formatter.red('Text')).to eq "\e[31mText\e[0m"
     end
 
     it 'returns the correct green string' do
       expect(formatter.green('Another Text')).to eq "\e[32mAnother Text\e[0m"
+    end
+
+    it 'returns the correct amber string' do
+      expect(formatter.amber('Text')).to eq "\e[33mText\e[0m"
+    end
+
+    it 'returns the correct blue string' do
+      expect(formatter.blue('Text')).to eq "\e[34mText\e[0m"
     end
   end
 end
