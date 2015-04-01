7328a72d3cd276132f485c83dd33cb63c13945a7
erwanlr <erwan.lr@gmail.com>
2015-04-01 20:08:48 +0100

    Adds spec for the global requests counter

--- spec/lib/cms_scanner_spec.rb
+++ spec/lib/cms_scanner_spec.rb
@@ -11,6 +11,28 @@ module CMSScanner
   end
 end
 
+describe CMSScanner do
+  let(:target_url) { 'http://wp.lab/' }
+
+  before do
+    scanner = CMSScanner::Scan.new
+    scanner.controllers.first.class.parsed_options = { url: target_url }
+  end
+
+  describe 'typhoeus_on_complete' do
+    before { CMSScanner.total_requests = 0 }
+
+    # TODO: find a way to test the cached requests which should not be counted
+    it 'returns the expected number of requests' do
+      stub_request(:get, /.*/)
+
+      CMSScanner::Browser.get(target_url)
+
+      expect(CMSScanner.total_requests).to eql 1
+    end
+  end
+end
+
 describe CMSScanner::Scan do
   subject(:scanner) { described_class.new }
   let(:controller)  { CMSScanner::Controller }
