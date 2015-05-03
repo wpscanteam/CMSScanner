662732ae3b78089dd50d73ce49f74f21a6ab681c
erwanlr <erwan.lr@gmail.com>
2015-05-03 20:34:16 +0100

    Uses #to_s in custom exceptions rather than #message

--- lib/cms_scanner/errors/http.rb
+++ lib/cms_scanner/errors/http.rb
@@ -1,21 +1,21 @@
 module CMSScanner
   # HTTP Authentication Required Error
   class HTTPAuthRequiredError < StandardError
-    def message
+    def to_s
       'HTTP authentication required (or was invalid), please provide it with --http-auth'
     end
   end
 
   # Proxy Authentication Required Error
   class ProxyAuthRequiredError < StandardError
-    def message
+    def to_s
       'Proxy authentication required (or was invalid), please provide it with --proxy-auth'
     end
   end
 
   # Access Forbidden Error
   class AccessForbiddenError < StandardError
-    def message
+    def to_s
       'The target is responding with a 403, this might be due to a WAF. ' \
       'Please re-try with --random-user-agent'
     end
