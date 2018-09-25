788b4e57c1272fda007cc026df3a57a46d019a83
erwanlr <erwan.lr@gmail.com>
2018-09-25 16:46:04 +0100

    Implements Typhoeus::Response#xml

--- lib/cms_scanner/typhoeus/response.rb
+++ lib/cms_scanner/typhoeus/response.rb
@@ -1,9 +1,14 @@
 module Typhoeus
   # Custom Response class
   class Response
-    # @return [ Nokogiri::HTML ] The response's body parsed by Nokogiri
+    # @return [ Nokogiri::HTML ] The response's body parsed by Nokogiri::HTML
     def html
       @html ||= Nokogiri::HTML(body.encode('UTF-8', invalid: :replace, undef: :replace))
+    end
+
+    # @return [ Nokogiri::XML ] The response's body parsed by Nokogiri::XML
+    def xml
+      @xml ||= Nokogiri::XML(body.encode('UTF-8', invalid: :replace, undef: :replace))
     end
   end
 end
