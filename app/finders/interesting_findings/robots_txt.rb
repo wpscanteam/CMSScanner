bc8484c6d5aba64a5ef848132527f5265d93d474
erwanlr <erwan.lr@gmail.com>
2019-03-26 19:30:35 +0000

    Uses head_and_get to check for the robots.txt

--- app/finders/interesting_findings/robots_txt.rb
+++ app/finders/interesting_findings/robots_txt.rb
@@ -5,18 +5,19 @@ module CMSScanner
     module InterestingFindings
       # Robots.txt finder
       class RobotsTxt < Finder
-        # @return [ String ] The url of the robots.txt file
-        def url
-          @url ||= target.url('robots.txt')
+        # @return [ String ] The path of the robots.txt file
+        def path
+          @path ||= 'robots.txt'
         end
 
         # @return [ InterestingFinding ]
         def aggressive(_opts = {})
-          res = NS::Browser.get(url)
+          # res = NS::Browser.get(url)
+          res = target.head_and_get(path)
 
           return unless res&.code == 200 && res.body =~ /(?:user-agent|(?:dis)?allow):/i
 
-          NS::Model::RobotsTxt.new(url, confidence: 100, found_by: found_by)
+          NS::Model::RobotsTxt.new(target.url(path), confidence: 100, found_by: found_by)
         end
       end
     end
--- spec/app/finders/interesting_findings/robots_txt_spec.rb
+++ spec/app/finders/interesting_findings/robots_txt_spec.rb
@@ -4,49 +4,40 @@ describe CMSScanner::Finders::InterestingFindings::RobotsTxt do
   subject(:finder) { described_class.new(target) }
   let(:target)     { CMSScanner::Target.new(url) }
   let(:url)        { 'http://example.com/' }
-  let(:robots_txt) { url + 'robots.txt' }
+  let(:robots_url) { url + 'robots.txt' }
   let(:fixtures)   { FIXTURES_FINDERS.join('interesting_findings', 'robots_txt') }
 
-  describe '#url' do
-    its(:url) { should eq robots_txt }
-  end
+  before { expect(finder.target).to receive(:head_or_get_params).and_return(method: :head) }
 
   describe '#aggressive' do
-    after do
-      stub_request(:get, robots_txt).to_return(status: status, body: body)
-
-      result = finder.aggressive
-
-      expect(result).to be_a CMSScanner::Model::RobotsTxt if @expected
-      expect(finder.aggressive).to eql @expected
-    end
-
-    let(:body) { '' }
+    before { stub_request(:head, robots_url).to_return(status: head_status) }
 
     context 'when 404' do
-      let(:status) { 404 }
+      let(:head_status) { 404 }
 
-      it 'returns nil' do
-        @expected = nil
-      end
+      its(:aggressive) { should eql nil }
     end
 
     context 'when 200' do
-      let(:status) { 200 }
+      let(:head_status) { 200 }
 
       context 'when the body is empty' do
         it 'returns nil' do
-          @expected = nil
+          stub_request(:get, robots_url).to_return(status: 200, body: '')
+
+          expect(finder.aggressive).to eql nil
         end
       end
 
       context 'when the body matches a robots.txt' do
-        let(:body) { File.read(fixtures.join('robots.txt')) }
-
         it 'returns the InterestingFinding result' do
-          @expected = CMSScanner::Model::RobotsTxt.new(robots_txt,
-                                                       confidence: 100,
-                                                       found_by: 'Robots Txt (Aggressive Detection)')
+          stub_request(:get, robots_url).to_return(status: 200, body: File.read(fixtures.join('robots.txt')))
+
+          expect(finder.aggressive).to eql CMSScanner::Model::RobotsTxt.new(
+            robots_url,
+            confidence: 100,
+            found_by: 'Robots Txt (Aggressive Detection)'
+          )
         end
       end
     end
