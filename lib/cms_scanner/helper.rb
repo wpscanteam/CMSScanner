dbbe6f8f4b69915000e611a8b965484b8d54bddc
Reelix <reelix+github@gmail.com>
2019-10-17 23:38:17 +0200

    Update helper.rb
    Code changes to make travis happy

--- lib/cms_scanner/helper.rb
+++ lib/cms_scanner/helper.rb
@@ -1,15 +1,13 @@
 # frozen_string_literal: true
 
-if (Gem.win_platform?)
+if Gem.win_platform?
   begin
     require 'sys/proctable'
   rescue LoadError
     print "Error: Unable to run wpscan\nPlease run 'gem install sys-proctable' for Windows machines\n"
     exit
   end
-  include Sys
 end
-
 
 # @param [ String ] file The file path
 def redirect_output_to_file(file)
@@ -19,8 +17,8 @@ end
 
 # @return [ Integer ] The memory of the current process in Bytes
 def memory_usage
-  if (Gem.win_platform?)
-    ProcTable.ps(pid: Process.pid).working_set_size
+  if Gem.win_platform?
+    Sys::ProcTable.ps(pid: Process.pid).working_set_size
   else
     `ps -o rss= -p #{Process.pid}`.to_i * 1024 # ps returns the value in KB
   end
