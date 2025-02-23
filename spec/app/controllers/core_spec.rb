d7f84f770ea7513e3caea35fc9c797b81afc4c5a
Erwan <erwanlr@users.noreply.github.com>
2025-02-23 14:51:53 -0500

    Fixes Rubocop Offenses

--- spec/app/controllers/core_spec.rb
+++ spec/app/controllers/core_spec.rb
@@ -152,9 +152,9 @@ describe CMSScanner::Controller::Core do
             it 'raises an error' do
               expect { core.before_scan }.to raise_error(
                 CMSScanner::Error::HTTPRedirect,
-                "The URL supplied redirects to #{redirection}." \
-                ' Use the --ignore-main-redirect option to ignore the redirection and scan the target,' \
-                ' or change the --url option value to the redirected URL.'
+                "The URL supplied redirects to #{redirection}. " \
+                'Use the --ignore-main-redirect option to ignore the redirection and scan the target, ' \
+                'or change the --url option value to the redirected URL.'
               )
             end
           end
